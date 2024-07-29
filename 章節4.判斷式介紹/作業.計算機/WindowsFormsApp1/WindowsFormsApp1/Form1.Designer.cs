dec22a6a4345d2a0d468ef291005ecc3133234db
ehmax <vsn80395@gmail.com>
2024-07-29 09:46:49 +0800

    start

---
+++ 章節4.判斷式介紹/作業.計算機/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(53, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(191, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(279, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 38);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "C";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(279, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 38);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(279, 253);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 40);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "=";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 515);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 章節4.判斷式介紹/作業.計算機/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        long total = 0;
+        bool isAdd = false;
+
+        long currentN = 0;//最後步驟
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isAdd)
+            {
+                textBox1.Clear();
+                isAdd = false;
+            }
+
+            textBox1.Text += "1";
+
+            currentN = Convert.ToInt64(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isAdd)
+            {
+                textBox1.Clear();
+                isAdd = false;
+            }
+
+            textBox1.Text += "2";
+
+            currentN = Convert.ToInt64(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isAdd)
+            {
+                textBox1.Clear();
+                isAdd = false;
+            }
+
+            textBox1.Text += "3";
+
+            currentN = Convert.ToInt64(textBox1.Text);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //作法1
+            //total += Convert.ToInt64(textBox1.Text);
+
+            total += currentN;
+
+
+            isAdd = true;
+
+
+
+            //textBox2
+
+            textBox1.Text = total.ToString();
+
+
+            currentN = 0;
+        }
+    }
+
+}
