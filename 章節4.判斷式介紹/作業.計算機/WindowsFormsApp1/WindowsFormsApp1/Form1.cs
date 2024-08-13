5029ee47522a3febabd77a6e24710db6ad0eaf3f
ehmax <vsn80395@gmail.com>
2024-08-13 11:28:04 +0800

    [更新] 計算機作業

--- 章節4.判斷式介紹/作業.計算機/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ 章節4.判斷式介紹/作業.計算機/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -14,79 +6,91 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
 
-        long total = 0;
-        bool isAdd = false;
+        decimal total = 0;
+        bool isAction = false;
+        bool start = false;
+        decimal currentN = 0;//最後步驟
 
-        long currentN = 0;//最後步驟
+        string tag = "";
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button6_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            if (isAdd)
+
+            if (start == false)
             {
                 textBox1.Clear();
-                isAdd = false;
+                start = true;
             }
 
-            textBox1.Text += "1";
-
-            currentN = Convert.ToInt64(textBox1.Text);
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (isAdd)
+            if (isAction)
             {
                 textBox1.Clear();
-                isAdd = false;
+                isAction = false;
             }
 
-            textBox1.Text += "2";
-
-            currentN = Convert.ToInt64(textBox1.Text);
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (isAdd)
-            {
-                textBox1.Clear();
-                isAdd = false;
-            }
-
-            textBox1.Text += "3";
+            textBox1.Text += ((Button)sender).Text;
 
             currentN = Convert.ToInt64(textBox1.Text);
 
+            if (total == 0)
+            {
+                total = currentN;
+            }
+            else
+            {
+                if (tag == "+")
+                {
+                    total += currentN;
+                }
+                else if (tag == "-")
+                {
+                    total -= currentN;
+                }
+                else if (tag == "x")
+                {
+                    total *= currentN;
+                }
+                else if (tag == "/")
+                {
+                    total /= currentN;
+                }
+            }
+
         }
+
+
+
 
         private void button5_Click(object sender, EventArgs e)
         {
-            //作法1
-            //total += Convert.ToInt64(textBox1.Text);
+            if (((Button)sender).Text != "=")
+            {
+                tag = ((Button)sender).Text;
+            }
 
-            total += currentN;
-
-
-            isAdd = true;
-
-
-
-            //textBox2
-
+            isAction = true;
             textBox1.Text = total.ToString();
-
-
             currentN = 0;
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            currentN = 0;
+            total = 0;
+            start = false;
+            tag = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
         }
     }
 
